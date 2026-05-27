3aaae9914e08097528db39ebe23398bab6567fb9
Nirupama Mallikarjunaiah <nmallika@usc.edu>
2026-05-27 00:24:57 -0700

    Add: subarray-product-less-than-k - submission-1

---
+++ Data Structures & Algorithms/subarray-product-less-than-k/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+       int i = 0;
+       int prod = 1;
+       int count = 0;
+
+       if( k <=1)
+        return 0;
+
+        for( int j = 0; j < nums.size(); ++j) {
+            prod *= nums[j];
+            while(i<=j && prod>=k){
+                prod /= nums[i];
+                i++;
+            }
+            count += (j-i+1);
+        }
+        return count;
+    }
+};
