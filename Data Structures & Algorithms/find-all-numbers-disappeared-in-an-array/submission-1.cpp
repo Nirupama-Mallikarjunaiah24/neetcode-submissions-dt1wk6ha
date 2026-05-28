cf328df45cffc11d190ef5e96713bed1b4335797
Nirupama Mallikarjunaiah <nmallika@usc.edu>
2026-05-28 15:48:56 -0700

    Add: find-all-numbers-disappeared-in-an-array - submission-1

---
+++ Data Structures & Algorithms/find-all-numbers-disappeared-in-an-array/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        
+        // Mark visited numbers
+        for (int i = 0; i < nums.size(); i++) {     
+            int idx = abs(nums[i]) - 1;
+            if (nums[idx] > 0) {
+                nums[idx] = -nums[idx];
+            }
+        }
+
+        // Collect missing numbers
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {           
+            if (nums[i] > 0) {
+                ans.push_back(i + 1);
+            }
+        }
+
+        return ans;
+    }
+};
