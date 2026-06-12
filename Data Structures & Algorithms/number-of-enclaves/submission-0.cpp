7972fa8c8142e8c485822405771b90736bb19076
Nirupama Mallikarjunaiah <nmallika@usc.edu>
2026-06-12 00:43:50 -0700

    Add: number-of-enclaves - submission-0

---
+++ Data Structures & Algorithms/number-of-enclaves/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int n,m;
+
+    void dfs (vector<vector<int>>& grid, int i, int j) {
+        if ( i>=0 && i < n && j >=0 && j < m && grid[i][j] == 1) {
+            grid[i][j] = 0; // mark visited
+            dfs(grid,i+1,j);
+            dfs(grid,i-1,j);
+            dfs(grid,i,j+1);
+            dfs(grid,i,j-1);
+        } else 
+            return;
+    }
+
+    int numEnclaves(vector<vector<int>>& grid) {
+        n = grid.size();
+        m = grid[0].size();
+
+        for(int i = 0 ; i <n; ++i) {
+            if (grid[i][0] == 1) dfs(grid,i,0);
+            if(grid[i][m-1] == 1) dfs(grid,i,m-1);
+        }
+
+        for ( int j = 0; j < m; ++j) {
+            if(grid[0][j] == 1) dfs(grid,0,j);
+            if (grid[n-1][j] == 1) dfs(grid,n-1,j);
+        }
+
+        int ans = 0;
+        for (int i = 0; i <n;++i) {
+            for (int j = 0; j<m; ++j) {
+                if(grid[i][j] == 1)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+
+};
